e51fcd6aff2ac907f330d333fbff73307a45df1c
Alejandro <alejandrozzzxx@gmail.com>
2024-11-13 18:08:55 -0600

    excluding unecesaries files

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express= require ('express');
+const cors = require('cors');
 const routerApi=require('./routes')
 
+
 const app = express();
+
 const port =3200;
+
+app.use(cors({ origin: 'http://127.0.0.1:5500' }));
 
 app.use(express.json());
 
